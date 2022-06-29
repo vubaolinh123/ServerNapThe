218bd171727651a91ad7b4644b573143faf3584c
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-06-29 18:17:00 +0700

    update chuyển trạng thái status thẻ, + Point vào game theo status

--- src/models/player.js
+++ src/models/player.js
@@ -8,7 +8,7 @@ const PlayerPoint = {
     // Lấy dữ liệu người chơi theo UUid có Point 
     getPointPlayerById: (username, callback) => {
         return db.query(`SELECT playerpoints_points.id, playerpoints_points.uuid, playerpoints_points.points, playerpoints_username_cache.username FROM playerpoints_points
-        INNER JOIN playerpoints_username_cache ON playerpoints_points.uuid=playerpoints_username_cache.uuid WHERE username=${username}`, callback)
+        INNER JOIN playerpoints_username_cache ON playerpoints_points.uuid=playerpoints_username_cache.uuid WHERE username='${username}'`, callback)
     },
     // Khi người chơi bấm nút Nạp thẻ, thông tin người chơi sẽ lưu lại để sau 30s sẽ check trạng thái nạp Thành Công, Thất Bại, Sai Thẻ,...
     AddPlayerNapThe: (info, callback) => {
@@ -18,8 +18,9 @@ const PlayerPoint = {
         return db.query(`SELECT * FROM 'trans_log' WHERE status = 0 AND trans_id = '${info.content}' AND pin = '${info.pin}'  AND seri = '${info.serial}' AND type = '${info.card_type}'`, callback)
     },
 
-    DoiTrangThaiNapThanhCong: (info, callback) => {
 
+    ThemXuChoNguoiChoiTrongGame: (info, callback) => {
+        return db.query(`UPDATE 'trans_log' SET 'status' = ${info.status} WHERE 'id'=${info.id}`)
     }
 
 }
--- src/routes/player.js
+++ src/routes/player.js
@@ -1,5 +1,6 @@
 import express from "express"
 const router = express.Router()
+import db from "../../Dbconnection"
 import PlayerPoint from "../models/player"
 import bodyParser from "body-parser";
 import { rateLimit } from "express-rate-limit";
@@ -7,7 +8,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // Chặn số lượng gửi Requested lên Server
 const apiLimiter = rateLimit({
-    windowMs: 80 * 1000, // 1 minutes
+    windowMs: 80 * 1000, // 80s được gửi 2 req
     max: 2,
     handler: function (req, res) {
         res.status(429).send({
@@ -21,7 +22,9 @@ const apiLimiter = rateLimit({
 // Lấy dữ liệu toàn bộ người chơi nạp Point hoặc 1 người chơi
 router.get('/:id?', (req, res, next) => {
     if (req.params.id) {
+        // Nếu truyền username lên url thì sẽ lấy 1 người chơi theo username
         PlayerPoint.getPointPlayerById(req.params.id, (err, rows) => {
+            console.log(req.params.id);
             if (err) {
                 res.status(400).json({
                     errors: "Người chơi đó không tồn tại hoặc lỗi gì đó?"
@@ -32,6 +35,7 @@ router.get('/:id?', (req, res, next) => {
             }
         })
     } else {
+        // nếu không truyền thì lấy hết dữ liệu
         PlayerPoint.getAllPointPlayer((err, rows) => {
             if (err) {
                 res.json(err);
@@ -46,8 +50,10 @@ router.get('/:id?', (req, res, next) => {
 router.post('/napthe', apiLimiter, (req, res, next) => {
     try {
         const { username, content, menhgia, mathe, seri, type } = req.body
+        // Nếu người dùng bấm form submit lên mà có các giá trị trên thì sẽ chạy vào đây
         if (username && content && menhgia && mathe && seri && type) {
             const info = req.body
+            // Lưu dữ liệu người chơi nạp thẻ vào DB riêng để sau này xác minh dựa trên dữ liệu content
             PlayerPoint.AddPlayerNapThe(info, (err, data) => {
                 if (err) {
                     res.status(400).json({
@@ -84,17 +90,34 @@ router.post('/', urlencodedParser, function (req, res, next) {
                 serial: serial,
                 card_type: card_type
             }
+            // Sau khi callback từ Server nạp thẻ trả về sẽ lấy dữ liệu người chơi trong DB riêng để đổi trạng thái và + Point
+            // Nếu trạng thái thành công
             PlayerPoint.LayThongTinNguoiNapThe(dataNapThe, (err, data) => {
                 if (err) {
-                    console.log(err);
+                    console.log("Không lấy được thông tin trong DB hoặc đối chiếu thông tin thất bại", err);
                     res.status(400).json({
                         err,
                         msg: "Đối chiếu thông tin người nạp thẻ thất bại"
                     });
                 }
                 else {
+                    // sau khi lấy thông tin người nạp thẻ xong thì check status từ req.body, nếu Ok thì cập nhật lại database
+                    // 1 Thành công và + Xu vào DB
+                    // 3 Sai mệnh giá, không + Xu nhưng vẫn khai báo lại đã nạp bao nhiêu tiền
+                    // 2 Thẻ nạp thất bại (sai mã thẻ hoặc gì đó)
                     console.log("dataPlayer", json(data));
-                    res.json(data);
+                    if (status === 'thanhcong') {
+                        // DB QUERY + POINT vào theo Name
+                        db.query(`UPDATE playerpoints_points
+                        INNER JOIN playerpoints_username_cache ON playerpoints_points.uuid=playerpoints_username_cache.uuid 
+                        SET playerpoints_points.points=playerpoints_points.points+(${amount * 0.001}) 
+                        WHERE playerpoints_username_cache.username='${data.name}'`)
+                        return db.query(`UPDATE 'trans_log' SET 'status' = 1 WHERE 'id'=${data.id}`)
+                    } else if (status === 'saimenhgia') {
+                        return db.query(`UPDATE 'trans_log' SET 'status' = 3, 'amount'=${amount} WHERE 'id'=${data.id}`)
+                    } else {
+                        return db.query(`UPDATE 'trans_log' SET 'status' = 2 WHERE 'id'=${data.id}`)
+                    }
                 }
             })
         } else {
